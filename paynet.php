81c9a22bd8f9facaeb264a4669bb87d1433da363
imenem <imenem@inbox.ru>
2013-07-01 21:33:59 +0300

    Now payment method "Paynet" can be selected for payment.

--- paynet.php
+++ paynet.php
@@ -10,12 +10,12 @@ if (!class_exists ('vmPSPlugin'))
 class plgVMPaymentPaynet extends vmPSPlugin
 {
     /**
-     * @param       JDispatcher     $subject        Joomla dispatcher
-     * @param       array           $config         Plugin config
+     * @param       JDispatcher     $dispatcher         Joomla dispatcher
+     * @param       array           $config             Plugin config
      */
-	function __construct (JDispatcher $subject, array $config)
+	public function __construct(JDispatcher $dispatcher, array $config)
     {
-		parent::__construct($subject, $config);
+		parent::__construct($dispatcher, $config);
 
 		$this->setConfigParameterable($this->_configTableFieldName, $this->getVarsToPush());
 	}
@@ -29,7 +29,7 @@ class plgVMPaymentPaynet extends vmPSPlugin
      *
      * @return      boolean
      */
-	function plgVmDeclarePluginParamsPayment($name, $id, TablePaymentmethods $table)
+	public function plgVmDeclarePluginParamsPayment($name, $id, TablePaymentmethods $table)
     {
 		return $this->declarePluginParams('payment', $name, $id, $table);
 	}
@@ -43,8 +43,54 @@ class plgVMPaymentPaynet extends vmPSPlugin
      *
 	 * @return      boolean
 	 */
-	function plgVmSetOnTablePluginParamsPayment($name, $id, TablePaymentmethods $table)
+	public function plgVmSetOnTablePluginParamsPayment($name, $id, TablePaymentmethods $table)
     {
 		return $this->setOnTablePluginParams($name, $id, $table);
+	}
+
+	/**
+	 * Check if the payment conditions are fulfilled for this payment method
+	 *
+     * @param       VirtueMartCart          $cart           Shopping cart
+     * @param       stdClass                $method         Payment method options
+	 * @param       array                   $prices         Shopping cart price list
+     *
+	 * @return      boolean
+	 */
+	protected function checkConditions($cart, $method, $prices)
+    {
+		return true;
+	}
+
+	/**
+	 * This event is fired to display the plugin methods in the cart
+	 *
+	 * @param       VirtueMartCart          $cart           Cart object
+	 * @param       integer                 $selected       ID of the method selected
+     * @param       array                   $htmlIn         Input html
+     *
+	 * @return      null|boolean                            True on succes, false on failures,
+     *                                                      null when this plugin was not selected.
+	 */
+	public function plgVmDisplayListFEPayment (VirtueMartCart $cart, $selected = 0, array &$htmlIn = array())
+    {
+		return $this->displayListFE($cart, $selected, $htmlIn);
+	}
+
+	/**
+	 * Calculate the price (value, tax_id) of the selected method.
+	 * It is called by the calculator.
+     *
+	 * @param       VirtueMartCart          $cart                   Cart object
+	 * @param       array                   $cartPrices             New cart prices
+	 * @param                               $cartPricesName         Varible for plugin tax html, fills by this method
+     *
+	 * @return      boolean|null                                    Null if the method was not selected,
+     *                                                              false if the shipping rate is not valid any more,
+     *                                                              true otherwise
+	 */
+	public function plgVmonSelectedCalculatePricePayment(VirtueMartCart $cart, array &$cartPrices, &$cartPricesName)
+    {
+		return $this->onSelectedCalculatePrice($cart, $cartPrices, $cartPricesName);
 	}
 }
